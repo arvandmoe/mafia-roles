c0b01ca784b60fe9e996b9f8da9ba0ae5e2560bb
Mohammad Khaki Bakhtiarvand <mdkhaki10@gmail.com>
2023-08-08 10:53:24 +0330

    add vazir font, improve ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -9,13 +10,19 @@ export const metadata: Metadata = {
   description: 'See users lists here',
 };
 
+const vazirFont = localFont({
+  src: './vazir.ttf',
+  display: 'auto',
+  variable: '--font-vazir',
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' dir='rtl' className={`${vazirFont.variable}`}>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import { DataTable } from '@/shared/components/data-table';
-import { Container } from '@/shared/components/layout/Container';
 import { Pagination } from '@/shared/components/pagination';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Container className='flex flex-col content-between'>
+    <main className='flex min-h-screen flex-col justify-between p-16 font-sans'>
         <DataTable />
         <Pagination />
-      </Container>
     </main>
   );
 }
